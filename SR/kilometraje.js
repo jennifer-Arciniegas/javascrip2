8c715b5f8bf43b6393cab96a1ca056fe11ae2f97
jennifer-Arciniegas <jennyarciniegas-28@hotmail.com>
2024-12-03 15:33:12 -0500

    ejercicio software review

---
+++ SR/kilometraje.js
@@ -0,0 +1,22 @@
+let name = prompt("ingrese su nombre");
+
+
+function ingresoInfoChofer(name, semana){
+    const choferes = {
+        nombre: name,
+        semana: []
+        /*{
+            lunes: "",
+            mates: "",
+            miercoles: "",
+            jueves: "",
+            viernes: "", 
+            sabado: ""
+        }*/
+
+    }
+
+
+
+}
+   
