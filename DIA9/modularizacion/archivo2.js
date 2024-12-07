22908d3d85eb6bc8e8e1ab45f4ed9675df0b0685
jennifer-Arciniegas <jennyarciniegas-28@hotmail.com>
2024-12-06 21:30:41 -0500

    dia 9 fechas y modulos

---
+++ DIA9/modularizacion/archivo2.js
@@ -0,0 +1,4 @@
+import{suma, PI} from './archivo1.js'
+
+console.log(suma(3, 2));
+console.log(PI);
---
+++ DIA9/modularizacion/archivo4.js
@@ -0,0 +1,4 @@
+import * as archivo from './archivo1.js'
+
+console.log(archivo.suma(4,5));
+console.log(archivo.PI)
